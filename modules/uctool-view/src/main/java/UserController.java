2295b9ecb8985a8ed0d00e0e2442445a24fef62e
manoj <manojg@wwso2.com>
2015-08-01 15:12:42 +0530

    Add uc-manager dependency to view

---
+++ modules/uctool-view/src/main/java/UserController.java
@@ -0,0 +1,11 @@
+import com.xtremenetworkx.UserManager;
+
+public class UserController {
+
+
+    public void getUsers(){
+
+        UserManager userManager = new UserManager();
+        userManager.Test();
+    }
+}
