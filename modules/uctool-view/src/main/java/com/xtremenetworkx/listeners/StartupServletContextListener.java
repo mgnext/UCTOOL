759989d28f82a4e5b635c5154d97323990952a00
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-08-03 09:39:02 +0530

    Servlet context listener added to put SSL verifying disabling code

---
+++ modules/uctool-view/src/main/java/com/xtremenetworkx/listeners/StartupServletContextListener.java
@@ -0,0 +1,37 @@
+package com.xtremenetworkx.listeners;
+
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author Xtreme
+ */
+public class StartupServletContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+//         javax.net.ssl.HttpsURLConnection.setDefaultAllowUserInteraction(true);
+//        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+//            new javax.net.ssl.HostnameVerifier(){
+//
+//                @Override
+//                public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+//                    return true;
+//                }
+//            });
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
